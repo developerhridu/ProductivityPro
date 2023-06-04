4eca867e8730552644d6fff9737c5133b45e6632
“[MR <mizanurrahmanhridu@gmail.com>
2023-06-04 17:41:42 +0600

    project structure added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+// import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+// import HomePage from "./pages/HomePage"
+
+const App = () => {
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default App;
