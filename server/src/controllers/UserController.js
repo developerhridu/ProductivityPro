9b71b4b88e0cc3bd0adecd3696bf22ce1a7dedc9
“[MR <mizanurrahmanhridu@gmail.com>
2023-06-08 20:02:57 +0600

    Token added on login controller

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,50 +1,34 @@
 const path = require('path');
 const fs = require('fs');
+const jwt = require("jsonwebtoken");
 
 const usersFilePath = path.join(__dirname, '../database/users.json');
 
 exports.registerUser = (req, res) => {
-    // Extract data from the request body
     const { firstName, lastName, email, password } = req.body;
-
-    // Validate form fields
     if (!firstName || !lastName || !email || !password) {
         return res.status(400).json({ error: 'Please fill in all fields.' });
     }
-
-    // Validate email format using regex
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
         return res.status(400).json({ error: 'Invalid email format.' });
     }
-
-    // Read the existing data from the JSON file
     fs.readFile(usersFilePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading data file:', err);
             return res.status(500).json({ error: 'Internal server error.' });
         }
-
-        // Parse the JSON data
         const users = JSON.parse(data);
-
-        // Check if the user already exists
         if (users.some((user) => user.email === email)) {
             return res.status(400).json({ error: 'User already exists.' });
         }
-
-        // Create a new user object
         const newUser = {
             firstName,
             lastName,
             email,
             password,
         };
-
-        // Add the new user to the array
         users.push(newUser);
-
-        // Write the updated data back to the JSON file
         fs.writeFile(usersFilePath, JSON.stringify(users), (err) => {
             if (err) {
                 console.error('Error writing data file:', err);
@@ -57,6 +41,7 @@ exports.registerUser = (req, res) => {
 };
 
 
+
 exports.loginUser = (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
@@ -67,17 +52,16 @@ exports.loginUser = (req, res) => {
             console.error('Error reading data file:', err);
             return res.status(500).json({ error: 'Internal server error.' });
         }
-
-        // Parse the JSON data
         const users = JSON.parse(data);
-
-        // Find the user with matching email and password
         const user = users.find((user) => user.email === email && user.password === password);
 
         if (!user) {
             return res.status(401).json({ error: 'Invalid email or password.' });
         }
+        const payload = { email: user.email };
+        const token = jwt.sign(payload, 'SecretKey123456789', { expiresIn: '24h' });
 
-        return res.status(200).json({ message: 'Login successful!' });
+        return res.status(200).json({ message: 'Login successful!', token });
     });
 };
+
