3e8e601c367176d0cb8aed780351c12e21549b62
“[MR <mizanurrahmanhridu@gmail.com>
2023-06-20 00:57:18 +0600

    updated

--- client/src/components/TaskTable.js
+++ client/src/components/TaskTable.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Table, Button } from 'react-bootstrap';
 import { ReadTaskRequest, DeleteTaskRequest } from '../APIRequest/APIRequest';
 
 const TaskTable = () => {
@@ -38,21 +39,21 @@ const TaskTable = () => {
     };
 
     return (
-        <div>
-            <div className="container mt-3">
-                <div className="row mt-3">
-                    <div className="col">
-                        <table className="table table-striped">
+        <div className="container mt-3">
+            <div className="row mt-3">
+                <div className="col-md-12">
+                    <div className="table-responsive word-wrap-break-word">
+                        <Table striped>
                             <thead>
                             <tr>
                                 <th></th>
-                                <th>Task Name</th>
-                                <th>Category</th>
-                                <th>Status</th>
-                                <th>Description</th>
-                                <th>Responsible Person</th>
-                                <th>Start Date</th>
-                                <th>End Date</th>
+                                <th style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>Task Name</th>
+                                <th style={{ maxWidth: '5%', overflow: 'hidden', wordWrap: 'break-word' }}>Category</th>
+                                <th style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>Status</th>
+                                <th style={{ maxWidth: '15%', overflow: 'hidden', wordWrap: 'break-word' }}>Description</th>
+                                <th style={{ maxWidth: '15%', overflow: 'hidden', wordWrap: 'break-word' }}>Responsible Person</th>
+                                <th style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>Start Date</th>
+                                <th style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>End Date</th>
                                 <th></th>
                                 <th></th>
                             </tr>
@@ -60,36 +61,38 @@ const TaskTable = () => {
                             <tbody>
                             {tasks.map((task) => (
                                 <tr key={task.taskID}>
-                                    <td><input type="checkbox" value="" /></td>
-                                    <td>{task.taskName || ''}</td>
-                                    <td>{task.taskCategory || ''}</td>
-                                    <td>{task.taskStatus || ''}</td>
-                                    <td>{task.taskDescription || ''}</td>
-                                    <td>{task.responsiblePerson || ''}</td>
-                                    <td>{task.startDate || ''}</td>
-                                    <td>{task.endDate || ''}</td>
+                                    <td style={{ whiteSpace: 'nowrap' }}>
+                                        <input type="checkbox" value="" />
+                                    </td>
+                                    <td style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>{task.taskName || ''}</td>
+                                    <td style={{ maxWidth: '5%', overflow: 'hidden', wordWrap: 'break-word' }}>{task.taskCategory || ''}</td>
+                                    <td style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>{task.taskStatus || ''}</td>
+                                    <td style={{ maxWidth: '15%', overflow: 'hidden', wordWrap: 'break-word' }}>{task.taskDescription || ''}</td>
+                                    <td style={{ maxWidth: '15%', overflow: 'hidden', wordWrap: 'break-word' }}>{task.responsiblePerson || ''}</td>
+                                    <td style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>{task.startDate || ''}</td>
+                                    <td style={{ maxWidth: '10%', overflow: 'hidden', wordWrap: 'break-word' }}>{task.endDate || ''}</td>
                                     <td>
-                                        <button className="btn btn-primary btn-sm" onClick={() => handleEditClick(task.taskID)}>
+                                        <Button variant="primary" size="sm" onClick={() => handleEditClick(task.taskID)}>
                                             <i className="fas fa-edit"></i>Edit
-                                        </button>
+                                        </Button>
                                     </td>
                                     <td>
-                                        <button className="btn btn-danger btn-sm" onClick={() => handleDeleteClick(task.taskID)}>
+                                        <Button variant="danger" size="sm" onClick={() => handleDeleteClick(task.taskID)}>
                                             <i className="fas fa-trash"></i>Delete
-                                        </button>
+                                        </Button>
                                     </td>
                                 </tr>
                             ))}
                             </tbody>
-                        </table>
+                        </Table>
                     </div>
                 </div>
-                <div className="row mt-3">
-                    <div className="col">
-                        <nav>
-                            <ul className="pagination" id="paginationContainer"></ul>
-                        </nav>
-                    </div>
+            </div>
+            <div className="row mt-3">
+                <div className="col">
+                    <nav>
+                        <ul className="pagination" id="paginationContainer"></ul>
+                    </nav>
                 </div>
             </div>
         </div>
